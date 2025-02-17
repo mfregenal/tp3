81eb4a70ce479edd4d7c5d68ff045d50430b080d
mfregenal <mfregenal23@gmail.com>
2025-02-17 10:21:25 -0300

    Segunda parte del tp3 - ejemplo

--- index.mjs
+++ index.mjs
@@ -1,6 +1,24 @@
-import {leerSuperheroes} from './utils.mjs';
+import {leerSuperheroes, agregarSuperheroes} from './utils.mjs';
+
+const archivoOriginal = './superheroes.txt';
+const archivoNuevo = './agregarSuperheroes.txt';
+let superheroes
 
 //Leer y mostrar la lista de superheroes ordenada
-const superheroes = leerSuperheroes('./superheroes.txt');
+superheroes = leerSuperheroes(archivoOriginal);
+
 console.log('Superheroes ordenados:');
+
+console.log(superheroes);
+
+//Agregar nuevos superhéroes
+console.log('===========================================================================================');
+console.log('Actualizando Superhéroes');
+agregarSuperheroes(archivoOriginal, archivoNuevo);
+
+//Leer y mostrar la lista actualizada de superhéroes ordenada
+superheroes = leerSuperheroes(archivoOriginal);
+
+console.log('Superhéroes ordenados: ');
+
 console.log(superheroes);
--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs'; //Proporciona una API para interactuar con el sistema de archivos, usando módulos de Node.js
+import { json } from 'stream/consumers';
 
 //Clase para representar un Superhéroe
 class Superheroe{
@@ -17,17 +18,46 @@ class Superheroe{
     }
 }
 
-//Función para leer y ordenar los superheroes
-export function leerSuperheroes(ruta){ //se crea una función "leerSuperheroes" y pasamos dirección de donde se leerán dichos superheroes
+//Función para leer y ordenar los superhéroes
+export function leerSuperheroes(ruta){ //Se crea una función "leerSuperheroes" y obtenemos como parámetro la dirección de donde se leerán dichos superhéroes
+
     const datos = fs.readFileSync(ruta, 'utf-8'); //Con readFileSync indicamos que se realice la lectura del archivo en la ruta indicada y que el código no siga su ejecución hasta terminar de leer dichos datos
+    
     const superheroesArray = JSON.parse(datos); //Analiza una cadena de texto como JSON (datos) y la transforma en un objeto de JavaScript (superheroesArray)
 
-    //Convertir a instancias de Superheroe
+    //Convertir a instancias de Superhéroe
     const superheroes = superheroesArray.map(
         hero => new Superheroe (hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo)
-    ); //Explicado en el archivo del "aclaraciones" del repositorio (1*)
+    ); //Explicado en el archivo "aclaraciones" del repositorio (1*)
 
     //Ordenar por nombre de superhéroe
-    superheroes.sort((a,b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe)); //Explicado en el archivo del "aclaraciones" del repositorio (2*)
-    return superheroes; //Retorna el array superheroes
+    superheroes.sort((a,b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe)); //Explicado en el archivo "aclaraciones" del repositorio (2*)
+
+    return superheroes; //Retorna el array superhéroes
+}
+
+//Nueva función para agregar superhéroes
+export function agregarSuperheroes(rutaOriginal, rutaNueva) {
+
+    //Realizamos la lectura de los archivos necesarios
+    const datosOriginales = fs.readFileSync(rutaOriginal, 'utf-8'); 
+    const datosNuevos = fs.readFileSync(rutaNueva, 'utf-8');
+
+    //Convertimos los archivos leídos a objetos JS
+    const superheroesOriginales = JSON.parse(datosOriginales);
+    const nuevosSuperheroes = JSON.parse(datosNuevos);
+
+    //Convertir los nuevos superhéroes a instancias de Superhéroe
+    const instanciasNuevos = nuevosSuperheroes.map(
+        hero => new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo)
+    );
+
+    //Combinar listas (utilizando Spread operator)
+    const listaActualizada = [...superheroesOriginales, ...instanciasNuevos];
+
+    //Guardar lista actualizada
+    fs.writeFileSync(rutaOriginal, JSON.stringify(listaActualizada, null, 2), 'utf-8'); //Explicado en el archivo "aclaraciones" del repositorio (3*)
+
+    console.log('Lista de superhéroes actualizada con éxito.');
+
 }
